02c8a89791bb447579d83d17831c9adc6d887815
nomuk984 <nomundari0804@gmail.com>
2024-10-03 23:50:37 +0800

    first commit

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ const deleteButton = document.querySelector("#delete-btn");
 let userMessage = null;
 let isResponseGenerating = false;
 
-const API_KEY = CONFIG.API_KEY || "API_KEY_PLACEHOLDER";
+const API_KEY = CONFIG.API_KEY;
 const API_URL = `https://generativelanguage.googleapis.com/v1/models/gemini-pro:generateContent?key=${API_KEY}`;
 
 const loadDataFromLocalstorage = () => {

8c6b46fd308839c0156ecde7934a3255c46b82ca
nomuk984 <nomundari0804@gmail.com>
2025-02-18 16:09:05 +0800

    halfway-checkpoint

---
+++ structures.js
@@ -0,0 +1,25 @@
+import { copyIcon, volumeUpIcon } from "./icons.js";
+
+export const getMessageStructure = (message, src) => {
+    return `
+    <div class="message-content">
+        <img src="${src}" />
+        <span>${message}</span>
+    </div>
+    <div class="message-actions hide">
+        ${copyIcon}
+        ${volumeUpIcon}
+    </div>
+    `;
+};
+
+export const loadingStructure = `
+    <div class="message-content">
+        <img class="avatar" src="./images/gemini.svg" />
+        <div class="loading-indicator">
+            <div class="loading-bar"></div>
+            <div class="loading-bar"></div>
+            <div class="loading-bar"></div>
+        </div>
+    </div>
+`;

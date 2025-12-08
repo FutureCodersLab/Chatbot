71d0bd93b004af4288d2daf7f2ecc3fd12cf7fe0
ThinkingTechDev <admin@thinkingtech.dev>
2025-12-08 21:58:12 +0800

    changed variable names

--- script.js
+++ script.js
@@ -123,13 +123,13 @@ const deleteChatHistory = () => {
 const toggleMode = () => {
     const isLightMode = container.classList.contains(lightModeText);
 
-    const currentMode = isLightMode ? darkModeText : lightModeText;
-    container.className = currentMode;
+    const newMode = isLightMode ? darkModeText : lightModeText;
+    container.className = newMode;
 
-    const nextMode = isLightMode ? lightModeText : darkModeText;
-    modeButton.innerText = nextMode;
+    const nextIcon = isLightMode ? lightModeText : darkModeText;
+    modeButton.innerText = nextIcon;
 
-    localStorage.setItem("mode", currentMode);
+    localStorage.setItem("mode", newMode);
 };
 
 const loadDataFromLocalStorage = () => {
@@ -137,11 +137,11 @@ const loadDataFromLocalStorage = () => {
     const savedMode = localStorage.getItem("mode");
 
     const currentMode = savedMode || lightModeText;
-    const nextMode =
+    const currentIcon =
         currentMode === lightModeText ? darkModeText : lightModeText;
 
     container.className = currentMode;
-    modeButton.innerText = nextMode;
+    modeButton.innerText = currentIcon;
 
     chatContainer.innerHTML = savedChatHistory || "";
     header.classList.toggle("hidden", savedChatHistory);
